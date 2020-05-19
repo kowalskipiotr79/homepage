fb192c3dd26d67c00bbdb995d6105c8396219c84
Piotr Kowalski <kowalskipiotr79@gmail.com>
2020-05-19 15:51:23 +0200

    tydzien 4 prawie gotowy

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ console.log(text1);
 console.log(text2);
 console.log("teksty1 = " + text1);
 console.log(`teskst2 = ${text2}`);
+
+function removeBackgroundColor(object) {
+  console.log("jestem w funkcji removeBackgrounColor");
+  //   console.log(object);
+  object.style.opacity = "0.5";
+}
